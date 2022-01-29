ff01c0d12891f03ec5a22fe5c269f7f77dcd468a
guominyingxiongququ <yxy1106@gmail.com>
2022-01-29 22:40:24 +0800

    add Predenoiser_RGB

--- models.py
+++ models.py
@@ -118,6 +118,81 @@ class ResidualBlock_noBN(nn.Module):
         out = self.conv2(out)
         return identity + out
 
+class Predenoiser_RGB(nn.Module):
+
+    def __init__(self, nf=64):
+        super(Predenoiser_RGB, self).__init__()
+
+        self.conv1_1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1)
+        self.conv1_2 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
+        self.pool1 = nn.MaxPool2d(kernel_size=2)
+        
+        self.conv2_1 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.conv2_2 = nn.Conv2d(128,128, kernel_size=3, stride=1, padding=1)
+        self.pool2 = nn.MaxPool2d(kernel_size=2)
+        
+        self.conv3_1 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
+        self.conv3_2 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+        self.pool3 = nn.MaxPool2d(kernel_size=2)
+        
+        self.conv4_1 = nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1)
+        self.conv4_2 = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
+        
+        self.upv5 = nn.ConvTranspose2d(512, 256, 2, stride=2)
+        self.conv5_1 = nn.Conv2d(512, 256, kernel_size=3, stride=1, padding=1)
+        self.conv5_2 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+        
+        self.upv6 = nn.ConvTranspose2d(256, 128, 2, stride=2)
+        self.conv6_1 = nn.Conv2d(256, 128, kernel_size=3, stride=1, padding=1)
+        self.conv6_2 = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
+        
+        self.upv7 = nn.ConvTranspose2d(128, 64, 2, stride=2)
+        self.conv7_1 = nn.Conv2d(128, 64, kernel_size=3, stride=1, padding=1)
+        self.conv7_2 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
+        
+        self.conv8_1 = nn.Conv2d(64, 3, kernel_size=1, stride=1)
+
+    def forward(self, x):
+
+        conv1 = self.lrelu(self.conv1_1(x))
+        conv1 = self.lrelu(self.conv1_2(conv1))
+        pool1 = self.pool1(conv1)
+        
+        conv2 = self.lrelu(self.conv2_1(pool1))
+        conv2 = self.lrelu(self.conv2_2(conv2))
+        pool2 = self.pool1(conv2)
+        
+        conv3 = self.lrelu(self.conv3_1(pool2))
+        conv3 = self.lrelu(self.conv3_2(conv3))
+        pool3 = self.pool1(conv3)
+        
+        conv4 = self.lrelu(self.conv4_1(pool3))
+        conv4 = self.lrelu(self.conv4_2(conv4))
+        
+        up5 = self.upv5(conv4)
+        up5 = torch.cat([up5, conv3], 1)
+        conv5 = self.lrelu(self.conv5_1(up5))
+        conv5 = self.lrelu(self.conv5_2(conv5))
+        
+        up6 = self.upv6(conv5)
+        up6 = torch.cat([up6, conv2], 1)
+        conv6 = self.lrelu(self.conv6_1(up6))
+        conv6 = self.lrelu(self.conv6_2(conv6))
+        
+        up7 = self.upv7(conv6)
+        up7 = torch.cat([up7, conv1], 1)
+        conv7 = self.lrelu(self.conv7_1(up7))
+        conv7 = self.lrelu(self.conv7_2(conv7))
+        
+        conv8= self.conv8_1(conv7)
+        out = conv8
+ 
+        return out
+
+    def lrelu(self, x):
+        out = torch.max(0.2*x, x)
+        return out
+        
 class Predenoiser(nn.Module):
 
     def __init__(self, nf=64):
